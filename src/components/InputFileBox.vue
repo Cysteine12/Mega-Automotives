bb528f92457d75d8740cce66896342031074b1fa
Cysteine12 <gbengapeace2001@gmail.com>
2025-01-07 16:42:03 +0100

    Refactor: renaming app-level components

---
+++ src/components/InputFileBox.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  photos: {
+    type: Array,
+    required: false,
+  },
+  isRequired: {
+    type: Boolean,
+    default: true,
+  },
+})
+const emit = defineEmits(['fileInput'])
+
+const currentPhotos = ref(props.photos)
+
+const handleFileChange = async (e) => {
+  const files = Array.from(e.target.files)
+
+  currentPhotos.value.push(files.map((file) => URL.createObjectURL(file)))
+
+  emit('fileInput', files)
+}
+</script>
+
+<template>
+  <div>
+    <div class="border border-gray w-100 p-1 min-height-100">
+      <img
+        v-for="(photo, index) in currentPhotos"
+        :key="index"
+        :src="photo"
+        alt=""
+        width="80px"
+        height="80px"
+        class="d-inline m-1"
+      />
+    </div>
+
+    <input
+      type="file"
+      @change="handleFileChange"
+      accept="image/*"
+      class="form-control form-file"
+      id="fileInputField"
+      max
+      multiple
+      :required="isRequired"
+    />
+  </div>
+</template>
+
+<style scoped>
+.form-control {
+  border-radius: 0;
+}
+
+.min-height-100 {
+  min-height: 100px;
+}
+</style>
