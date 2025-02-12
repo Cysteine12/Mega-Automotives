8f16ec1b908d2d5a1210aeb428a0eb32646d0eb6
Cysteine12 <gbengapeace2001@gmail.com>
2025-02-12 20:31:41 +0100

    fix(cloudinary): add verify file type

--- src/composables/handleFileChange.js
+++ src/composables/handleFileChange.js
@@ -1,7 +1,14 @@
 import * as fileService from '@/services/fileService'
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
 
 const handleFileChange = async (folder, file, currentFileUrl = null) => {
   if (!file) return null
+  if (!file.type.includes('image')) {
+    toast.error('Please upload a valid picture')
+    throw new Error('Not a picture')
+  }
 
   const paramsToSign = {
     eager: 'c_pad,h_300,w_400|c_crop,h_200,w_260',
